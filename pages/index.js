6c99235cde8254bb3e95447c706bde5eb60d2b9a
kidlatmc29 <kidlatmc@gmail.com>
2023-05-28 22:55:02 -0700

    using public folder import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import headshot from '/main_pfp.jpg'
+import headshot from '../public/main_pfp.jpg'
 
 export default function Home() {
   return (
